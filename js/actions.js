2f5ce97593043b6a9bcd104d56abe3f2fada2605
Weapon45 <luisorlando.itsx@gmail.com>
2016-04-02 11:45:43 -0600

    version 9

--- js/actions.js
+++ js/actions.js
@@ -16,7 +16,7 @@ var fn = {
     // --- FUNCIONES DE REGISTRO ---
     estaRegistrado: function(){
 		var usr = window.localStorage.getItem("user");
-		if(usr == undefined || usr == ''){			
+		if(usr == undefined || usr ==''){			
         	return false;
 		}
 		else{
@@ -33,13 +33,11 @@ var fn = {
         
         // -- COMPROBAMOS SI LOS CAMPOS NO ESTAN VACIOS --
         if(nom != '' && mail != '' && tel != '' && foto != undefined)
-			alert("entro");
            $.ajax({
 			  method: "POST",
 			  url: "http://carlos.igitsoft.com/apps/test.php",
 			  data: { nom: nom, mail: mail, tel: tel }
 			}).done(function( msg ) {
-			   alert(msg);
 				if(msg == 1){
 					ft.transfer(foto);
 				}
